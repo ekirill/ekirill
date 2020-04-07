1d022c85e7322b6ad40b8a217ce2058f38300d72
Kirill Emelyanov <ekirill@ekirill.ru>
2020-04-07 23:56:23 +0300

    google oauth2 settings to env

--- ekirill/core/config.py
+++ ekirill/core/config.py
@@ -26,12 +26,26 @@ class Camera:
             self.videodir = env('VIDEODIR')
 
 
+class GoogleOAuthSettings:
+    def __init__(self, env):
+        with env.prefixed('GOOGLE_OAUTH2_'):
+            self.key = env('KEY', '')
+            self.secret = env('SECRET', '')
+
+
+class Auth:
+    def __init__(self, env):
+        with env.prefixed('AUTH_'):
+            self.google = GoogleOAuthSettings(env)
+
+
 class AppConfig:
     def __init__(self, env=default_env):
         with env.prefixed('EKIRILL_'):
             self.secret = env('SECRET', 'devsecret')
             self.db = DB(env)
             self.camera = Camera(env)
+            self.auth = Auth(env)
 
 
 app_config = AppConfig()
